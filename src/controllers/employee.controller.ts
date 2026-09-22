69bb63b6410f3c69cb21dbcce2825a658edaf970
AnitaDSM-155 <anahiperezdsm@gmail.com>
2026-09-22 17:46:46 -0300

    terminamos routes una vez agregamos los handlers ahora si ya hechos para poder utiliarlos

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import { EmployeeService } from "../services/employee.service";
 
-//controller trabaja la traduccion http req,res, no valida ni calcula ya que eso lo hace service
+//controller recibe req,res, no valida ni calcula ya que eso lo hace service, lo que hacemos nosotros es pasarle los datos a service
 export class EmployeeController {
     constructor(private readonly service: EmployeeService) { } //inyeccion de dependencias, no creamos un service, lo traemos de afuera que vendria siendo nuestro employeeService
-
+    //segun lo que el service devuelva o tire como error, arma una respuesta http correcta
     handleErrors(error: any, res: Response) {
         if (error.statusCode) {
             return res.status(error.statusCode).json({ message: error.message });
--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -1,14 +1,24 @@
-import { Router } from "express"
+import { Router } from "express";
+import { EmployeeController } from "../controllers/employee.controller";
+import { EmployeeService } from "../services/employee.service";
+import { EmployeeRepository } from "../repository/employee.repository";
 
-export class employeeRoutes {
+export class EmployeeRoutes {
 
     static get routes(): Router {
 
-       const router = Router()
+        const router = Router();
 
-       router.post('/employees')
-       router.get('/employees')
-       router.get('/employees/:id')
+        // aca arma la cadena completa de inyección de dependencias e instanciamos todo
+        // primero el repository, con eso arma el service, con eso arma el controller posteriormente
+        const repository = new EmployeeRepository();
+        const service = new EmployeeService(repository);
+        const controller = new EmployeeController(service);
 
-    return router
-}}
+        router.post('/employees', controller.create);
+        router.get('/employees', controller.findAll);
+        router.get('/employees/:id', controller.findOne);
+
+        return router;
+    }
+}
